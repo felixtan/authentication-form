310c70a2286bf0c557da63dcabdb7697a7bf941d
felixtan <tanf91@gmail.com>
2017-02-09 02:42:08 -0500

    dirty and validity checking events for input element styling

--- submitOnEnter.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const signupForm = document.getElementById('signupForm')
-  signupForm.addEventListener('keypress', e => {
-    if (e.keyCode === 13 && signupForm.checkValidity()) {
-      signupForm.submit()
-    }
-  })
-})()
