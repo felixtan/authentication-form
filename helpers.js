460d86700f55d7317291900e42d59cb66c450f9c
felixtan <tanf91@gmail.com>
2017-02-10 23:04:04 -0500

    sanitize all input before submission for sign up form l

--- helpers.js
+++ helpers.js
@@ -1,17 +1,22 @@
 (() => {
-
-  const form = document.getElementById('signupForm')
+  const form = document.getElementsByTagName('form')[0]
 
   form.addEventListener('submit', () => {
-    form.password.value = sanitize(form.password.value)
+    const inputs = document.getElementsByTagName('input')
+
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i]
+      const sanitize = input.name === 'password'
+        ? encodeURIComponentEnhanced
+        : encodeURIComponent
+      input.value = sanitize(input.value)
+    }
   })
 
-  // extend encodeURI to encode "(" and ")"
-  // use this to sanitize user input
-  function sanitize(input) {
-    return encodeURI(input)
+  // extend encodeURIComponent to encode "(" and ")"
+  function encodeURIComponentEnhanced(input) {
+    return encodeURIComponent(input)
             .replace(/\(/g, '%28')
             .replace(/\)/g, '%29')
-            .replace(/\//g, '%2F')
   }
 })()
