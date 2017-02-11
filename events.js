23df8da6e3b8af81c6976d5d9ceb3c10fb9c6cc9
felixtan <tanf91@gmail.com>
2017-02-11 18:15:20 -0500

    live validation styling

--- events.js
+++ events.js
@@ -13,22 +13,42 @@
     for (let i = 0; i < inputs.length; i++) {
       const input = inputs[i]
 
+      const validationMessageContainer = document.querySelector(`input#${input.id} + span.validation-message`)
+      const label = document.querySelector(`label[for=${input.id}]`)
+
+      // Skip show password checkbox
+      if (validationMessageContainer === null) continue
+
+      const validMessageContainer = validationMessageContainer.children[0]
+      const invalidMessageContainer = validationMessageContainer.children[1]
+
       input.addEventListener('focusout', () => {
         const valid = input.validity.valid
 
-        // Dirty?
+        // Valid?
         if (input.classList.contains('dirty-input')) {
           if (!valid) {
             input.classList.remove('valid-input')
             input.classList.add('invalid-input')
+            invalidMessageContainer.style.visibility = 'visible'
+            validMessageContainer.style.visibility = 'hidden'
+            label.style.color = 'red'
           } else {
             input.classList.remove('invalid-input')
             input.classList.add('valid-input')
+            invalidMessageContainer.style.visibility = 'hidden'
+            validMessageContainer.style.visibility = 'visible'
+            label.style.color = 'black'
+          }
+
+          if (input.classList.contains('empty-input') && input.classList.contains('optional')) {
+            invalidMessageContainer.style.visibility = 'hidden'
+            validMessageContainer.style.visibility = 'hidden'
           }
         }
       })
 
-      // Valid?
+      // Dirty?
       input.addEventListener('input', () => {
         if (!input.classList.contains('dirty-input')) {
           input.classList.add('dirty-input')
