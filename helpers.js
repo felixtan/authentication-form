86a061eb2d4e59d121bea9719f270f52b5e7a3fb
felixtan <tanf91@gmail.com>
2017-02-11 01:04:23 -0500

    added show password checkbox

--- helpers.js
+++ helpers.js
@@ -1,4 +1,8 @@
 (() => {
+
+  /**
+   *  Sanitize inputs upon submission
+   */
   const form = document.getElementsByTagName('form')[0]
 
   form.addEventListener('submit', () => {
@@ -19,4 +23,25 @@
             .replace(/\(/g, '%28')
             .replace(/\)/g, '%29')
   }
+
+  /**
+   *  Show password
+   */
+  const showPasswordCheck = document.getElementById('showPassword')
+  const showPasswordLabel = document.getElementById('showPasswordLabel')
+  const password = document.getElementById('signupPassword')
+
+  // For convenience on mobile since checkbox might be too small
+  showPasswordLabel.addEventListener('click', () => {
+    showPasswordCheck.checked = !showPasswordCheck.checked
+
+    // For some reason, the checkbox's onchange event is not triggered
+    // when the label is clicked, even though its value does change.
+    password.type = showPasswordCheck.checked ? 'text' : 'password'
+  })
+
+  showPasswordCheck.addEventListener('change', () => {
+    password.type = showPasswordCheck.checked ? 'text' : 'password'
+  })
+
 })()
