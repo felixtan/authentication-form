38946ca1c45981e7dc474ed46498f0a7c4f89830
felixtan <tanf91@gmail.com>
2017-02-25 19:54:54 -0500

    opened https server, redirect all traffic under http to https instead

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
@@ -6,13 +9,19 @@ const sql = require('sql.js')
 const hbs = require('express-hbs')
 const encodeURIComponentEnhanced = require('./scripts/helpers').encodeURIComponentEnhanced
 
+// HTTPS
+const certDirectory = path.join(__dirname, "../../.localhost-ssl/")
+const serverOptions = {
+  key: fs.readFileSync(path.join(certDirectory, 'key.pem')),
+  cert: fs.readFileSync(path.join(certDirectory, 'certificate.pem'))
+}
+
 // Database
 const db = new sql.Database()
 db.run('CREATE TABLE users (name char, company char, email char, password char);')
 
 // Express
 const app = express()
-const PORT = 3000
 
 // Templating
 app.engine('hbs', hbs.express4({
@@ -25,8 +34,9 @@ app.set('views', __dirname + '/views');
 // Middleware
 const passport = require('./scripts/auth.js')(db)
 app.use(express.static(__dirname + '/'))
+app.use(redirectToHttps)
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(printRequestMiddleware)
+app.use(printRequestMiddleware)
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
@@ -39,9 +49,18 @@ app.use('/login', routers.login)
 app.use('/forgotpw', routers.forgotpw)
 app.use('/logout', routers.logout)
 
-app.listen(PORT)
+app.listen(3000)
+https.createServer(serverOptions, app).listen(3001)
 
 function printRequestMiddleware(req, res, next) {
   console.log(`\nHeaders:\n${JSON.stringify(req.headers, null, 4)}\n\nBody:\n${JSON.stringify(req.body, null, 4)}`)
+  next()
+}
+
+function redirectToHttps(req, res, next) {
+  if (req.protocol === 'http') {
+    return res.status(301).redirect(`https://${req.hostname}:3001${req.path}`)
+  }
+
   next()
 }
